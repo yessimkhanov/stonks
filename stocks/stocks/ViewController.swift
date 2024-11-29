083578d2022abea789f10d54ab07c203351340e3
aldiyar <aldiyar.yessimkhanov@nu.edu.kz>
2024-11-29 22:02:24 +0500

    commit of empty project

---
+++ stocks/stocks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stocks
+//
+//  Created by Nursultan Turekulov on 29.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
